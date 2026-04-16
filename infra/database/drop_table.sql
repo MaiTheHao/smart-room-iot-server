a6fb4540342c0fec92d67d874d8c032549832ea3
MaiTheHao <anhchangnhanma167@gmail.com>
2026-04-16 09:42:40 +0700

    refactor: remove room_id from rule_v2 and clean up database migration scripts and schema definitions

--- infra/database/drop_table.sql
+++ infra/database/drop_table.sql
@@ -1,113 +1,69 @@
 USE smart_room_iot;
 
-SET
-  FOREIGN_KEY_CHECKS = 0;
+SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
--- Xóa bảng Module System
+-- Module: System & Auth
 -- ----------------------------
 DROP TABLE IF EXISTS `client`;
-
 DROP TABLE IF EXISTS `client_group`;
-
 DROP TABLE IF EXISTS `sys_group`;
-
 DROP TABLE IF EXISTS `sys_group_lan`;
-
 DROP TABLE IF EXISTS `sys_function`;
-
 DROP TABLE IF EXISTS `sys_function_lan`;
-
 DROP TABLE IF EXISTS `sys_role`;
-
 DROP TABLE IF EXISTS `persistent_logins`;
-
 DROP TABLE IF EXISTS `language`;
 
 -- ----------------------------
--- Xóa bảng Module Core
+-- Module: Core Domain
 -- ----------------------------
 DROP TABLE IF EXISTS `floor`;
-
 DROP TABLE IF EXISTS `floor_lan`;
-
 DROP TABLE IF EXISTS `room`;
-
 DROP TABLE IF EXISTS `room_lan`;
-
 DROP TABLE IF EXISTS `device_control`;
 
 -- ----------------------------
--- Xóa bảng Module Devices
+-- Module: Devices & Sensors
 -- ----------------------------
 DROP TABLE IF EXISTS `air_condition`;
-
 DROP TABLE IF EXISTS `air_condition_lan`;
-
 DROP TABLE IF EXISTS `light`;
-
 DROP TABLE IF EXISTS `light_lan`;
-
 DROP TABLE IF EXISTS `temperature`;
-
 DROP TABLE IF EXISTS `temperature_lan`;
-
 DROP TABLE IF EXISTS `temperature_value`;
-
 DROP TABLE IF EXISTS `power_consumption`;
-
 DROP TABLE IF EXISTS `power_consumption_lan`;
-
 DROP TABLE IF EXISTS `power_consumption_value`;
-
 DROP TABLE IF EXISTS `fan`;
-
 DROP TABLE IF EXISTS `fan_lan`;
 
 -- ----------------------------
--- Xóa bảng Module Automation
+-- Module: Automation & Rules
 -- ----------------------------
 DROP TABLE IF EXISTS `automation`;
-
 DROP TABLE IF EXISTS `automation_action`;
-
--- ----------------------------
--- Xóa bảng Module Rule
--- ----------------------------
 DROP TABLE IF EXISTS `rule`;
-
 DROP TABLE IF EXISTS `rule_condition`;
-
 DROP TABLE IF EXISTS `rule_v2`;
-
 DROP TABLE IF EXISTS `rule_condition_v2`;
-
 DROP TABLE IF EXISTS `rule_action_v2`;
 
 -- ----------------------------
--- Xóa bảng Module Quartz
+-- Module: Quartz Scheduler
 -- ----------------------------
 DROP TABLE IF EXISTS `QRTZ_BLOB_TRIGGERS`;
-
 DROP TABLE IF EXISTS `QRTZ_CALENDARS`;
-
 DROP TABLE IF EXISTS `QRTZ_CRON_TRIGGERS`;
-
 DROP TABLE IF EXISTS `QRTZ_FIRED_TRIGGERS`;
-
 DROP TABLE IF EXISTS `QRTZ_JOB_DETAILS`;
-
 DROP TABLE IF EXISTS `QRTZ_LOCKS`;
-
 DROP TABLE IF EXISTS `QRTZ_PAUSED_TRIGGER_GRPS`;
-
 DROP TABLE IF EXISTS `QRTZ_SCHEDULER_STATE`;
-
 DROP TABLE IF EXISTS `QRTZ_SIMPLE_TRIGGERS`;
-
 DROP TABLE IF EXISTS `QRTZ_SIMPROP_TRIGGERS`;
-
 DROP TABLE IF EXISTS `QRTZ_TRIGGERS`;
 
-SET
-  FOREIGN_KEY_CHECKS = 1;
+SET FOREIGN_KEY_CHECKS = 1;
--- infra/database/truncate.sql
+++ infra/database/truncate.sql
@@ -1,110 +1,69 @@
 USE smart_room_iot;
 
-SET
-  FOREIGN_KEY_CHECKS = 0;
+SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
 -- Module: System & Auth
 -- ----------------------------
 TRUNCATE TABLE `client`;
-
 TRUNCATE TABLE `client_group`;
-
 TRUNCATE TABLE `sys_group`;
-
 TRUNCATE TABLE `sys_group_lan`;
-
 TRUNCATE TABLE `sys_function`;
-
 TRUNCATE TABLE `sys_function_lan`;
-
 TRUNCATE TABLE `sys_role`;
-
 TRUNCATE TABLE `persistent_logins`;
-
 TRUNCATE TABLE `language`;
 
 -- ----------------------------
--- Module: Core Domain (Building & Devices)
+-- Module: Core Domain
 -- ----------------------------
 TRUNCATE TABLE `floor`;
-
 TRUNCATE TABLE `floor_lan`;
-
 TRUNCATE TABLE `room`;
-
 TRUNCATE TABLE `room_lan`;
-
 TRUNCATE TABLE `device_control`;
 
 -- ----------------------------
 -- Module: Devices & Sensors
 -- ----------------------------
 TRUNCATE TABLE `air_condition`;
-
 TRUNCATE TABLE `air_condition_lan`;
-
 TRUNCATE TABLE `light`;
-
 TRUNCATE TABLE `light_lan`;
-
 TRUNCATE TABLE `fan`;
-
 TRUNCATE TABLE `fan_lan`;
-
 TRUNCATE TABLE `temperature`;
-
 TRUNCATE TABLE `temperature_lan`;
-
 TRUNCATE TABLE `temperature_value`;
-
 TRUNCATE TABLE `power_consumption`;
-
 TRUNCATE TABLE `power_consumption_lan`;
-
 TRUNCATE TABLE `power_consumption_value`;
 
 -- ----------------------------
--- Module: Automation
+-- Module: Automation & Rules
 -- ----------------------------
 TRUNCATE TABLE `automation`;
-
 TRUNCATE TABLE `automation_action`;
-
 TRUNCATE TABLE `rule`;
-
 TRUNCATE TABLE `rule_condition`;
-
 TRUNCATE TABLE `rule_v2`;
-
 TRUNCATE TABLE `rule_condition_v2`;
-
 TRUNCATE TABLE `rule_action_v2`;
 
 -- ----------------------------
 -- Module: Quartz Scheduler
 -- ----------------------------
 TRUNCATE TABLE `QRTZ_BLOB_TRIGGERS`;
-
 TRUNCATE TABLE `QRTZ_CALENDARS`;
-
 TRUNCATE TABLE `QRTZ_CRON_TRIGGERS`;
-
 TRUNCATE TABLE `QRTZ_FIRED_TRIGGERS`;
-
 TRUNCATE TABLE `QRTZ_JOB_DETAILS`;
-
 TRUNCATE TABLE `QRTZ_LOCKS`;
-
 TRUNCATE TABLE `QRTZ_PAUSED_TRIGGER_GRPS`;
-
 TRUNCATE TABLE `QRTZ_SCHEDULER_STATE`;
-
 TRUNCATE TABLE `QRTZ_SIMPLE_TRIGGERS`;
-
 TRUNCATE TABLE `QRTZ_SIMPROP_TRIGGERS`;
-
 TRUNCATE TABLE `QRTZ_TRIGGERS`;
 
-SET
-  FOREIGN_KEY_CHECKS = 1;
+SET FOREIGN_KEY_CHECKS = 1;
