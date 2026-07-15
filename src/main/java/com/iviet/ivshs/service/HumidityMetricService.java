8a16b7d1989f3ba0a76c5373e7ea3d19f3a1c672
MaiTheHao <anhchangnhanma167@gmail.com>
2026-07-15 23:25:32 +0700

    feat: create HumidityMetricService interface

---
+++ src/main/java/com/iviet/ivshs/service/HumidityMetricService.java
@@ -0,0 +1,11 @@
+package com.iviet.ivshs.service;
+
+import com.iviet.ivshs.service.strategy.MetricServiceStrategy;
+import com.iviet.ivshs.service.strategy.SensorTelemetryServiceStrategy;
+import com.iviet.ivshs.service.strategy.TelemetryCRUDServiceStrategy;
+
+/**
+ * Service interface for handling Humidity Metrics, extending the platform's strategies.
+ */
+public interface HumidityMetricService extends TelemetryCRUDServiceStrategy, MetricServiceStrategy, SensorTelemetryServiceStrategy {
+}
