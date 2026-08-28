bc78bfcae5a2696a5a5fa457e362a8761aae897a
MaiTheHao <anhchangnhanma167@gmail.com>
2026-08-29 03:08:58 +0700

    fix: update room event handler to use REQUIRES_NEW

--- src/main/java/com/iviet/ivshs/service/RoomEventHandler.java
+++ src/main/java/com/iviet/ivshs/service/RoomEventHandler.java
@@ -26,6 +26,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
@@ -44,7 +45,7 @@ public class RoomEventHandler {
   private final AlertTriggerService alertTriggerService;
 
   @Async
-  @Transactional
+  @Transactional(propagation = Propagation.REQUIRES_NEW)
   @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
   public void handleRoomEvent(RoomEventApplicationEvent event) {
     if (event == null || event.getRoomId() == null || event.getEventCode() == null) {
